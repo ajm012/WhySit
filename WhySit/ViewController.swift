29f6535bfde6bb4c1eb36e45da3cd9143c13b74a
Andy <andrewmcconnell2016@u.northwestern.edu>
2017-04-25 11:28:37 -0500

    Initial Commit

---
+++ WhySit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhySit
+//
+//  Created by Andrew McConnell on 4/25/17.
+//  Copyright © 2017 Andrew McConnell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
