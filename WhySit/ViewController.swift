f9568d68a4fcfd6bf352e09897684e08f066cc98
Andy <andrewmcconnell2016@u.northwestern.edu>
2017-05-09 21:54:04 -0500

    aded toast messaging, background location tracking, scanning for BLE services

--- WhySit/ViewController.swift
+++ WhySit/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreLocation
 import ResearchKit
 import CoreBluetooth
-
+//import GoogleAPIClientForREST
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
@@ -20,8 +20,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var longitude: UILabel!
     //@IBOutlet weak var tableView: UITableView!
     
+    let arrayOfServices: [CBUUID] = [CBUUID(string: "8CB88C3B-E0B0-4448-B1D3-EE073DDA5941")]
+    let state = UIApplication.shared.applicationState
+    
     var centralManager: CBCentralManager?
     var peripherals = Array<CBPeripheral>()
+    
+    var count = 1
     
     var locationManager: CLLocationManager!
     
@@ -30,15 +35,19 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         centralManager = CBCentralManager(delegate: self, queue: DispatchQueue.main)
+        survey.isEnabled = false
         submit.isEnabled = false
-        //print("View did load")
+        
+        print("View did load")
+        
         
     }
     
     override func viewWillAppear(_ animated: Bool) {
+
         super.viewWillAppear(animated)
-        
         determineMyCurrentLocation()
+        
     }
     
     func determineMyCurrentLocation() {
@@ -46,6 +55,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestAlwaysAuthorization()
+        locationManager.allowsBackgroundLocationUpdates = true
         
         if CLLocationManager.locationServicesEnabled() {
             locationManager.startUpdatingLocation()
@@ -54,15 +64,16 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
         let userLocation:CLLocation = locations[0] as CLLocation
         
         // Call stopUpdatingLocation() to stop listening for location updates,
         // other wise this function will be called every time when user location changes.
         
         //manager.stopUpdatingLocation()
-        
         //print("user latitude = \(userLocation.coordinate.latitude)")
         //print("user longitude = \(userLocation.coordinate.longitude)")
+        
         //csvText += "\(userLocation.coordinate.latitude)" + ","
         //csvText += "\(userLocation.coordinate.longitude)" + "\n"
         longitude.text = String(format: "%f", userLocation.coordinate.longitude)
@@ -79,6 +90,26 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func showToast(message : String) {
+        
+        print("Creating toast")
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 100, y: self.view.frame.size.height/2-100, width: 200, height: 150))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.textAlignment = .center;
+        toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 8.0, delay: 0.1, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
     }
     
     @IBAction func consentTapped(sender : AnyObject) {
@@ -104,7 +135,8 @@ extension ViewController : ORKTaskViewControllerDelegate {
         if (taskViewController.task?.identifier == "SurveyTask" && reason == .completed) {
             print("Starting results")
             
-            let fileName = "Survey"
+            let fileName = "Survey" + String(count)
+            count += 1
             let DocumentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
             
             let fileURL = DocumentDirURL.appendingPathComponent(fileName).appendingPathExtension("csv")
@@ -201,8 +233,6 @@ extension ViewController : ORKTaskViewControllerDelegate {
             }
             csvText += latitude.text! + ","
             csvText += longitude.text! // + "\n"
-    
-            //print(csvText)
             
             do {
                 // Write to the file
@@ -210,16 +240,6 @@ extension ViewController : ORKTaskViewControllerDelegate {
             } catch let error as NSError {
                 print("Failed writing to URL: \(fileURL), Error: " + error.localizedDescription)
             }
-            
-            /*var readString = "" // Used to store the file contents
-            do {
-                // Read the file contents
-                readString = try String(contentsOf: fileURL)
-            } catch let error as NSError {
-                print("Failed reading from URL: \(fileURL), Error: " + error.localizedDescription)
-            }
-            print("File Text: \(readString)")*/
-            
             
         }
             taskViewController.dismiss(animated: true, completion: nil)
@@ -245,8 +265,9 @@ extension ViewController : ORKTaskViewControllerDelegate {
 extension ViewController: CBCentralManagerDelegate {
 
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
+
         if (central.state == .poweredOn){
-            print("Scanning for peripherals")
+            //print("here")
             self.centralManager?.scanForPeripherals(withServices: nil, options: nil)
         }
         else {
@@ -255,11 +276,37 @@ extension ViewController: CBCentralManagerDelegate {
     }
     
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        print("Scanning for peripherals")
+ 
         if (peripheral.name != nil) {
+            showToast(message: "Survey requested!")
             print("Peripheral discovered")
             print(peripheral.name! as Any)
+            print("\(advertisementData)".components(separatedBy: "\n")[0]) //.valueForKey("kCBAdvDataIsConnectable"))
+            //if ((peripheral.name!.range(of: "Andrew’s MacBook Pro")) != nil) { // may need to fix name
             peripherals.append(peripheral)
+            print(peripheral.services as Any)
+            print("Connecting...")
+                //if watch...centralManager.connectPeripheral(peripheral, options: nil)
+            centralManager?.connect(peripheral, options: nil)
+            print(peripheral.identifier)
+            centralManager?.stopScan()
+            survey.isEnabled = true
+                //peripheral.readValue(for: a) //CBCharacteristic??*/
+            //}
         }
+    }
+    
+    // Called when connection succeeded
+    func centralManager(central: CBCentralManager,
+                        didConnectPeripheral peripheral: CBPeripheral) {
+        print("Connected!")
+    }
+    // Called when connection failed
+    func centralManager(_ central: CBCentralManager,
+                        didFailToConnect peripheral: CBPeripheral,
+                        error: Error?) {
+        print("Failed…")
     }
 }
 
